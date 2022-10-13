f9a4cfd436558d63a87609ce0b06d07bb379188a
Mikkel <mtkongsted@gmail.com>
2022-10-13 10:20:00 +0200

    work description + deleted solution

--- frontend/post.js
+++ frontend/post.js
@@ -2,14 +2,8 @@
 const input = document.getElementById("post-value")
 const postBtn = document.getElementById("post-button")
 
-    
-
 postBtn.addEventListener("click", async function() {
     const dataToSend = {post: input.value}
-    const body = JSON.stringify(dataToSend);
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    const method = "POST"
-    await fetch("api/createpost", {body, headers, method})
+    
 
 })
